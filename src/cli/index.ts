3e65e7ffabb5bc4b8f9b97c83c810b5ae427837d
beatoz-ryan <ryan.park@beatoz.io>
2025-12-26 13:05:54 +0900

    add CLI exports and update package.json for new module structure

---
+++ src/cli/index.ts
@@ -0,0 +1,8 @@
+/** @format */
+
+export { Chaincode as CliChaincode } from "./chaincode"
+export { CliChaincodeDeployer, PackagingMode } from "./cli-chaincode-deployer"
+export { CliChaincodeInit } from "./cli-chaincode-init"
+export { CliChaincodeInvoker } from "./cli-chaincode-invoker"
+export { CliChaincodePackageCreater } from "./cli-chaincode-package-creater"
+export { Cli, PeerCli } from "./peer-cli"
